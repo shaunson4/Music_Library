83fa0815d2d5baa6c9e03dfdac23212900fa3ce8
shaunson4 <sgray4106@gmail.com>
2022-05-06 08:55:26 -0500

    Inititated Music_Library project into repository + Created project folders + started project in django + Created local_setting file + transfered Revised Database settings and Secret_key

---
+++ music_library/local_setting.py
@@ -0,0 +1,16 @@
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-e!3av#0v(bh^+ar=m-1n7b&bl51t5%gi!ju@mdu!5yx$y(9xn8'
+
+# Database
+# https://docs.djangoproject.com/en/4.0/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.msql',
+        'NAME': 'music_library_database',
+        'HOST': 'localhost',
+        'USER': 'root',
+        'PASSWORD': 'password'
+    }
+}
+
